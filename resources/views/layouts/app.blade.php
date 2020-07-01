35362e1be80b69cca42df7a746ba91a59bc83601
Soulyaris <genessa174@gmail.com>
2020-07-01 18:47:06 +0500

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('page-title')</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    @include('includes.header')
+
+    @yield('content')
+
+    @include('includes.footer')
+</body>
+    <script src="/js/jquery-3.5.1.slim.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="/js/jquery.slim.min.js"><\/script>')</script><script src="/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
+</html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts/app')
+
+@section('page-title')
+    SMArt Project
+@endsection
+
+@section('content')
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, quis voluptatibus similique repudiandae aspernatur quasi architecto, sit expedita ut nostrum distinctio provident voluptas voluptatem, aperiam possimus fugit temporibus aut cupiditate?</p>
+@endsection
