0073fc8f4adecc0b793ebf99a01791b02bac23c5
Soulyaris <genessa174@gmail.com>
2020-08-02 18:54:20 +0500

    added gallery filtration

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CategoryModel as Category;
 use App\Models\ImageModel as Image;
+use DateTime;
 use Illuminate\Http\Request;
 
 class GalleryController extends Controller
@@ -10,15 +12,36 @@ class GalleryController extends Controller
     public function index(Request $request) {
 
         $validatedData = $request->validate([
-            'name' => 'string|min:2|max:120',
+            'name' => 'nullable|string|min:2|max:120',
+            'category' => 'nullable|integer',
+            'showlast' => 'nullable|string',
         ]);
 
+        $categories = Category::where('isActive', true)->orderBy('name', 'asc')->get();
 
         $images = Image::where('isActive', true)->when($request->name, function ($query, $requestName) {
             if ($requestName):
                 return $query->where('name', 'ILIKE', '%'.$requestName.'%');
             endif;
-        })->paginate(50);
-        return view('gallery', ['images' => $images]);
+        })->when($request->showlast, function ($query, $showLast) {
+            $currentDate = new DateTime();
+            $previousDate = new DateTime();
+            switch ($showLast):
+                case "day":
+                    $previousDate->modify('-1 day'); break;
+                case "week":
+                    $previousDate->modify('-1 week'); break;
+                case "month":
+                    $previousDate->modify('-1 month'); break;
+                case "year":
+                    $previousDate->modify('-1 year'); break;
+            endswitch;
+            return $query->whereBetween('created_at', [$previousDate->format('Y-m-d H:i:s'), $currentDate->format('Y-m-d H:i:s')]);
+        })->when($request->category, function ($query, $requestCategory) {
+            if ($requestCategory):
+                return $query->where('category', $requestCategory);
+            endif;
+        })->orderBy('created_at', $request->sortmethod ? "desc" : "asc")->paginate(50);
+        return view('gallery', ['images' => $images, 'categories' => $categories]);
     }
 }
--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -8,9 +8,12 @@
     <div class="container">
         <div class="card">
             <h5 class="card-header mb-3">SMArt project gallery</h5>
-            <form class="form-inline mt-2 mt-md-0 card-body pt-0 pb-0" method="POST" action="{{ route('gallery') }}">
+            <form class="form-inline mt-2 mt-md-0 card-body pt-0 pb-0" method="POST" action="{{ route('gallery') }}" id="gallery-filter-form">
                 <div class="input-group w-100 mb-3">
                     @csrf
+                    <div class="input-group-prepend">
+                        <button id="filter-openner" class="btn btn-outline-primary" type="button">Filter</button>
+                    </div>
                     <input type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Image name" name="name" id="name" aria-label="name" aria-describedby="basic-addon2">
                     <div class="input-group-append">
                       <button class="btn btn-outline-secondary" type="submit">Search</button>
@@ -20,6 +23,44 @@
                             <strong>{{ $message }}</strong>
                         </span>
                     @enderror
+                </div>
+                <div id='gallery-filter' class='container border border-top-0 rounded'>
+                    <h5>Filter</h5>
+                    <div class="filter-group border rounded mb-2" id="show-last">
+                        <div class="filter-group-header bg-secondary">By date</div>
+                        <div class="filter-group-body">
+                            <input class="" type="radio" id="show-last-day" name="showlast" value="day">
+                            <label for="show-last-day">Show last day</label>
+                            <input class="" type="radio" id="show-last-week" name="showlast" value="week">
+                            <label for="show-last-week">Show last week</label>
+                            <input class="" type="radio" id="show-last-month" name="showlast" value="month">
+                            <label for="show-last-month">Show last month</label>
+                            <input class="" type="radio" id="show-last-year" name="showlast" value="year">
+                            <label for="show-last-year">Show last year</label>
+                        </div>
+                    </div>
+                    <div class='filter-group border rounded mb-2' id='sort-row'>
+                        <div class="filter-group-header bg-secondary">Sort type</div>
+                        <div class="filter-group-body">
+                            <input type="checkbox" name="sortmethod" id="sortmethod">
+                            <label for="sortmethod">Sort by date</label>
+                        </div>
+                    </div>
+                    <div class="filter-group border rounded mb-2" id="category">
+                        <div class="filter-group-header bg-secondary">By category</div>
+                        <div class="filter-group-body">
+                            @foreach ($categories as $category)
+                                <input class="" type="radio" id="category{{ $category->id }}" name="category" value="{{ $category->id}}">
+                                <label for="category{{ $category->id }}">{{ $category->name }}</label>
+                            @endforeach
+                        </div>
+                    </div>
+                    <div class="filter-group input-group w-100">
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('Filter') }}
+                        </button>
+                        <a class="btn btn-primary" href="{{ route('gallery') }}">Clear</a>
+                    </div>
                 </div>
             </form>
             <div id="image-gallery">
@@ -37,4 +78,18 @@
             {{ $images->links() }}
         </div>
     </div>
+    <script>
+        window.addEventListener("DOMContentLoaded", function(event) {
+            var filterBtn = $( '#filter-openner' ).first();
+            var galleryFilter = $( '#gallery-filter' ).first();
+
+            filterBtn.on('click', function (e) {
+                e.preventDefault();
+
+                filterBtn.toggleClass('active');
+                galleryFilter.toggleClass('active');
+            });
+
+        });
+    </script>
 @endsection
