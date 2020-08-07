9e151fe888197797e84f14973a8137aca60f8fd1
Soulyaris <genessa174@gmail.com>
2020-08-07 13:20:53 +0500

    added custom 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('page-title')
+    SMArt Project 404
+@endsection
+
+@section('content')
+    <div id="not-found-body">
+        <div class="flex-center position-ref full-height">
+            <div class="code">404</div>
+
+            <div class="message" style="padding: 10px;">Not Found</div>
+        </div>
+    </div>
+@endsection
